f5aff18c26cee954c24bb7bcf6c668fc95b89bc2
ddatunashvili <davitidatunashvili98@gmail.com>
2021-09-09 01:41:01 +0400

    calculating with error

--- script.js
+++ script.js
@@ -5,43 +5,53 @@ var screen = document.querySelector('#capscreen');
 
 
 // ოპერაციები
-var a=0
-var b=0
-var equal=0
-var ope = 0
+var operation="";
+// ready for capture second number
+var ready = false;
 
-function operation(oper){  
-    if (a == 0 ){
-        a = screen.textContent
-        ope = oper
-    }else{
-        b = screen.textContent
-    }
-    console.log(a,b,)
-}
+// first input
+var first = ""
 
-// equals
-function equals(){
-    if (ope == "*"){
-        equal = a*b
-    }
-    screen.innerHTML = equal
-    ope = 0
-    a = 0
-    b = 0
-}
-
-
-
+// showing on screen
 function get_value(value){
-    if (ope !=0){
-        screen.innerHTML=""
+
+    // clean if its equals writen
+    if (screen.textContent == first){
+        screen.innerHTML="" 
     }
 
+    // clean if it has zero value
     if (screen.textContent == "0"){
         screen.innerHTML=""
     }
-    screen.innerHTML += value
+
+    screen.innerHTML+=value;
+
+    
+    // if we type second value
+    if (ready == true){
+        operation +=`${parseInt(screen.textContent)}`  
+    } 
 }
 
+// * / + -
+function calc(oper){  
+    // შენახვა ცვლადში
+    first=screen.textContent
+    operation+=`${parseInt(screen.textContent)}${oper}`
+    screen.innerHTML=`${screen.textContent}`
+    ready=true
+}
+
+
+// equals
+function equal(){
+    if (operation !=""){
+        console.log(operation,"result");
+        screen.innerHTML = eval(operation)
+        // update
+        ready=false
+        operation=""
+    }
+}
 
