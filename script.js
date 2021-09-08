8c0142c637e44d350b73615175509e1bf06189f2
gio1971 <giooorgi7@gmail.com>
2021-09-08 21:16:58 +0400

    added operation and enter first letter

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-
 // წვდომა
 var screen = document.querySelector('#capscreen');
 
 
-
 // ოპერაციები
-var a=0
-var b=0
-var equal=0
-var ope = 0
-
-function operation(oper){  
-    if (a == 0 ){
-        a = screen.textContent
-        ope = oper
-    }else{
-        b = screen.textContent
-    }
-    console.log(a,b,)
-}
-
-// equals
-function equals(){
-    if (ope == "*"){
-        equal = a*b
-    }
-    screen.innerHTML = equal
-    ope = 0
-    a = 0
-    b = 0
-}
-
+var a="";
+var b="";
+var c="";
+var f = 1;
+var equal=0;
+var nextm = 0;
+var operation = "";
 
 
 function get_value(value){
-    if (ope !=0){
-        screen.innerHTML=""
+    if (f){
+        screen.innerHTML="";
+        screen.textContent = value;
+        f = 0;
+        console.log('f = '+f);
     }
+    else if(screen.textContent != "0"){
+        screen.textContent += value;
+        console.log('fff = '+f);
 
-    if (screen.textContent == "0"){
-        screen.innerHTML=""
     }
-    screen.innerHTML += value
 }
+
+
+function get_operation(oper){  
+    f = 1;
+    if (a == "" && operation == ""){
+        a = screen.textContent;
+        operation = oper;
+        console.log('f_operation = '+f);
+        // console.log(type(operation));
+        console.log(operation);
+        console.log("------------------");
+        // console.log(type(a));
+        console.log(a);
+        console.log("------------------");
+    }else{
+        b = screen.textContent;
+        c = a+operation+b;
+        total = eval(c)
+        console.log(c);
+        screen.innerHTML = total
+        a = total
+        operation = oper;
+    }
+    // console.log(a,b,)
+}
+
+// // equals
+// function equals(){
+//     if (ope == "*"){
+//         equal = a*b
+//     }
+//     screen.innerHTML = equal
+//     ope = 0
+//     a = 0
+//     b = 0
+// }
+
+
 
 
